fec4f8356b3e7fbdf77f99a1d4ff17358c23fc47
Katie Maxwell <katiemaxwell10@hotmail.com>
2017-06-15 14:22:43 +0100

    swiper gallery

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+ $(document).ready(function () {
+ 	var galleryTop = new Swiper('.gallery-top', {
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        spaceBetween: 10,
+    });
+    var galleryThumbs = new Swiper('.gallery-thumbs', {
+        spaceBetween: 10,
+        centeredSlides: true,
+        slidesPerView: 'auto',
+        touchRatio: 0.2,
+        slideToClickedSlide: true
+    });
+
+    galleryTop.params.control = galleryThumbs;
+    galleryThumbs.params.control = galleryTop;
+ });
+
  $(".accordion").on("click", ".accordion-header", function() {
  	$(this).toggleClass("active").next().slideToggle();
  });
