406bc432f45c7afb89f289181240b19706bb5cba
Katie Maxwell <katiemaxwell10@hotmail.com>
2017-06-23 14:45:50 +0100

    layout and accordion list changes

--- script.js
+++ script.js
@@ -1,21 +1,48 @@
  $(document).ready(function () {
- 	var galleryTop = new Swiper('.gallery-top', {
-        nextButton: '.swiper-button-next',
-        prevButton: '.swiper-button-prev',
-        spaceBetween: 10,
-    });
-    var galleryThumbs = new Swiper('.gallery-thumbs', {
-        spaceBetween: 10,
-        centeredSlides: true,
-        slidesPerView: 'auto',
-        touchRatio: 0.2,
-        slideToClickedSlide: true
-    });
+ 	// var galleryTop = new Swiper('.gallery-top', {
+  //       nextButton: '.swiper-button-next',
+  //       prevButton: '.swiper-button-prev',
+  //       spaceBetween: 10,
+  //   });
+  //   var galleryThumbs = new Swiper('.gallery-thumbs', {
+  //       spaceBetween: 10,
+  //       centeredSlides: true,
+  //       slidesPerView: 'auto',
+  //       touchRatio: 0.2,
+  //       slideToClickedSlide: true
+  //   });
 
-    galleryTop.params.control = galleryThumbs;
-    galleryThumbs.params.control = galleryTop;
- });
+  //   galleryTop.params.control = galleryThumbs;
+  //   galleryThumbs.params.control = galleryTop;
 
- $(".accordion").on("click", ".accordion-header", function() {
- 	$(this).toggleClass("active").next().slideToggle();
- });
+  $(".box").hide(0).fadeIn(3000);
+  $(".card").hide(0).fadeIn(3000);
+
+ //  $( function() {
+ //    $( "#accordion" ).accordion({
+ //      collapsible: true
+ //    });
+ //  } );
+ // });
+
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].onclick = function(){
+
+        this.classList.toggle("active");
+
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    }
+}
+});
+ // $(".accordion").on("click", ".accordionheader", function() {
+ // 	$(this).toggleClass("active").next().slideToggle();
+ // });
