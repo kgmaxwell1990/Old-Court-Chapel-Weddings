9dfb1630317355cf3a464bbc474b47fe91e63714
Katie Maxwell <katiemaxwell10@hotmail.com>
2017-07-08 15:59:06 +0100

    layout

--- script.js
+++ script.js
@@ -1,48 +1,28 @@
  $(document).ready(function () {
- 	// var galleryTop = new Swiper('.gallery-top', {
-  //       nextButton: '.swiper-button-next',
-  //       prevButton: '.swiper-button-prev',
-  //       spaceBetween: 10,
-  //   });
-  //   var galleryThumbs = new Swiper('.gallery-thumbs', {
-  //       spaceBetween: 10,
-  //       centeredSlides: true,
-  //       slidesPerView: 'auto',
-  //       touchRatio: 0.2,
-  //       slideToClickedSlide: true
-  //   });
-
-  //   galleryTop.params.control = galleryThumbs;
-  //   galleryThumbs.params.control = galleryTop;
 
   $(".box").hide(0).fadeIn(3000);
   $(".card").hide(0).fadeIn(3000);
 
- //  $( function() {
- //    $( "#accordion" ).accordion({
- //      collapsible: true
- //    });
- //  } );
- // });
+
+  $("#carousel").carousel();
 
 
-var acc = document.getElementsByClassName("accordion");
-var i;
+  var acc = document.getElementsByClassName("accordion");
+  var i;
 
-for (i = 0; i < acc.length; i++) {
-    acc[i].onclick = function(){
+  for (i = 0; i < acc.length; i++) {
+      acc[i].onclick = function(){
 
-        this.classList.toggle("active");
+          this.classList.toggle("active");
 
-        var panel = this.nextElementSibling;
-        if (panel.style.display === "block") {
-            panel.style.display = "none";
-        } else {
-            panel.style.display = "block";
-        }
-    }
-}
+          var panel = this.nextElementSibling;
+          if (panel.style.display === "block") {
+              panel.style.display = "none";
+          } else {
+              panel.style.display = "block";
+          }
+      }
+  }
 });
- // $(".accordion").on("click", ".accordionheader", function() {
- // 	$(this).toggleClass("active").next().slideToggle();
- // });
+
+
