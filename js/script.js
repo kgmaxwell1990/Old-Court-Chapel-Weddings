4c4afd893523b7b2ca8ead67fde08143c10d702f
Katie Maxwell <katiemaxwell10@hotmail.com>
2017-08-20 15:50:37 +0100

    code layout

--- js/script.js
+++ js/script.js
@@ -1,35 +1,35 @@
- $(document).ready(function () {
+var acc = document.getElementsByClassName("accordion");
+var i;
 
-  $(".card").hide(0).fadeIn(3000);
+for (i = 0; i < acc.length; i++) {
+    acc[i].onclick = function(){
 
+        this.classList.toggle("active");
 
-  
-  var acc = document.getElementsByClassName("accordion");
-  var i;
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    }
+}
 
-  for (i = 0; i < acc.length; i++) {
-      acc[i].onclick = function(){
+function initMap() {
+    var options = {
+      zoom:8,
+      center:{lat:54.367288,lng:-5.560291}
+    }
 
-          this.classList.toggle("active");
+    var map = new
+    google.maps.Map(document.getElementById('map'), options);
 
-          var panel = this.nextElementSibling;
-          if (panel.style.display === "block") {
-              panel.style.display = "none";
-          } else {
-              panel.style.display = "block";
-          }
-      }
-  }
-
-
-});
+    var marker = new google.maps.Marker({
+      position:{lat:54.367288,lng:-5.560291},
+      map:map
+    });
+}
 
 function myFunction() {
     alert("Your information was received & we will get back to you as soon as possible!");
 }
-
-
-
-
-
-
